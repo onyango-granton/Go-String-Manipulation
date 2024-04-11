2bf5e1cdd752380d2e79427716638e0ef3dbe725
gonyango <grantononyango@gmail.com>
2024-04-11 07:02:11 +0300

    Hello

---
+++ Functions/test.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+	"strings"
+	"strconv"
+	//"unicode"
+)
+
+func hexToDec(hexStr string) string {
+	decValue, err := strconv.ParseInt(hexStr, 16, 64)
+	if err != nil {
+		return hexStr
+	}
+	return strconv.FormatInt(decValue, 10)
+}
+
+func binToDec(binStr string) string {
+	decValue, err := strconv.ParseInt(binStr, 2, 64)
+	if err != nil {
+		return binStr
+	}
+	return strconv.FormatInt(decValue, 10)
+}
+
+func processText(input string) string {
+	words := strings.Fields(input)
+	for i, word := range words {
+		switch {
+		case strings.Contains(word, "(hex)"):
+			if i > 0 {
+				words[i-1] = hexToDec(words[i-1])
+			}
+			words = append(words[:i], words[i+1:]...)
+			i-- // Adjust index after removal
+		case strings.Contains(word, "(bin)"):
+			if i > 0 {
+				words[i-1] = binToDec(words[i-1])
+			}
+			words = append(words[:i], words[i+1:]...)
+			i-- // Adjust index after removal
+		case strings.Contains(word, "(up)"):
+			if i > 0 {
+				words[i-1] = strings.ToUpper(words[i-1])
+			}
+			words = append(words[:i], words[i+1:]...)
+			i-- // Adjust index after removal
+		// Implement other rules (low, cap, punctuation, ' ,a) here
+		}
+	}
+	return strings.Join(words, " ")
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: go run . <input_file> <output_file>")
+		os.Exit(1)
+	}
+
+	inputFile := os.Args[1]
+	outputFile := os.Args[2]
+
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Println("Error opening input file:", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var processedLines []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		processedLine := processText(line)
+		processedLines = append(processedLines, processedLine)
+	}
+
+	output, err := os.Create(outputFile)
+	if err != nil {
+		fmt.Println("Error creating output file:", err)
+		os.Exit(1)
+	}
+	defer output.Close()
+
+	writer := bufio.NewWriter(output)
+	for _, line := range processedLines {
+		fmt.Fprintln(writer, line)
+	}
+	writer.Flush()
+}
---
+++ Team/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func isSpecialword(s string) bool {
+	count := 0
+	boolChar := []bool{}
+	characters := []string{".", ","}
+	for _, word := range s {
+		for _, c := range characters {
+			if c == string(word) {
+				boolChar = append(boolChar, true)
+			} else {
+				boolChar = append(boolChar, false)
+			}
+		}
+	}
+
+	for _, bc := range boolChar {
+		if bc {
+			count++
+		}
+	}
+
+	if count > 1 {
+		return true
+	}
+
+	return false
+}
+
+func formatSentence(s string) string {
+	words := strings.Split(s, " ")
+	for i, word := range words {
+		if isSpecialword(word) && i > 0 {
+			//words[i - 1] = ""
+			words[i-1] = words[i-1] + words[i]
+			words = append(words[:i], words[i+1:]...)
+		}
+	}
+
+	s2 := strings.Join(words, " ")
+
+	return s2
+}
+
+func main() {
+	s1 := "Hello ... world"
+	s2 := strings.Split(s1, " ")
+	for _, word := range s2 {
+		fmt.Println(isSpecialword(word))
+	}
+	fmt.Println(formatSentence(s1))
+}
