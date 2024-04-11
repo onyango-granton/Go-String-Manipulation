ff2c8a4c17bde8986a3e7d31313bdb514ec17097
gonyango <grantononyango@gmail.com>
2024-04-11 07:20:40 +0300

    Hello

--- Team/main.go
+++ Team/main.go
@@ -8,7 +8,7 @@ import (
 func isSpecialword(s string) bool {
 	count := 0
 	boolChar := []bool{}
-	characters := []string{".", ","}
+	characters := []string{".", ",", "!", "?", ":"}
 	for _, word := range s {
 		for _, c := range characters {
 			if c == string(word) {
@@ -25,7 +25,7 @@ func isSpecialword(s string) bool {
 		}
 	}
 
-	if count > 1 {
+	if count > 0 {
 		return true
 	}
 
@@ -47,8 +47,29 @@ func formatSentence(s string) string {
 	return s2
 }
 
+func isMark(s string) bool {
+	marks := []string{".", ",", "!", "?", ":"}
+	for _, mark := range marks {
+		if mark == s {
+			return true
+		}
+	}
+	return false
+}
+
+func formatPunctuation(word string) string {
+	words := strings.Split(word, " ")
+	for i, word := range words{
+		if isMark(word) && i > 0 {
+			words[i - 1] = words[i - 1] + words[i]
+			words = append(words[:i], words[i+1:]...)
+		}
+	}
+	return strings.Join(words, " ")
+}
+
 func main() {
-	s1 := "Hello ... world"
+	s1 := "Hello . world"
 	s2 := strings.Split(s1, " ")
 	for _, word := range s2 {
 		fmt.Println(isSpecialword(word))
