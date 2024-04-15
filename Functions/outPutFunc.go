70cc92bb865477513a65f021980f0935b36d36e2
gonyango <grantononyango@gmail.com>
2024-04-15 16:38:50 +0300

    Vidi

--- Functions/outPutFunc.go
+++ Functions/outPutFunc.go
@@ -3,11 +3,11 @@ package go_reloaded
 func OutputFunc(s1 string) (string, error) {
 	s1 = BinToDec(s1)
 	s1 = HexToDec(s1)
-	s1 = GroupPunctations(s1)
-	s1 = SinglePunctuations(s1)
 	s1 = Capitalize(s1)
 	s1 = UpperCase(s1)
 	s1 = LowerCase(s1)
+	s1 = GroupPunctations(s1)
+	s1 = SinglePunctuations(s1)
 	s1 = WordStartPunc(s1)
 	s1 = PairSpeechMark(s1)
 	s1 = PunctuateVowel(s1)
--- Functions/speechMark.go
+++ Functions/speechMark.go
@@ -1,32 +1,42 @@
 package go_reloaded
 
-import "strings"
+import (
+	"regexp"
+	//"strings"
+)
 
-func isSpeechMark(s string) bool {
-	speechMark := "'"
-	return speechMark == s
-}
+// func isSpeechMark(s string) bool {
+// 	speechMark := "'"
+// 	return speechMark == s
+// }
+
+// func PairSpeechMark(s string) string {
+// 	// words := strings.Split(s, " ")
+// 	// isStartSpeechMark := false
+// 	// for index, word := range words {
+// 	// 	if isSpeechMark(word) {
+// 	// 		isStartSpeechMark = !isStartSpeechMark
+// 	// 		if isStartSpeechMark && index-1 > 0 && index+1 < len(words) {
+// 	// 			words[index] = words[index] + words[index+1]
+// 	// 			words = append(words[:index+1], words[index+2:]...)
+// 	// 		} else if !isStartSpeechMark {
+// 	// 			if index+1 < len(words) {
+// 	// 				words[index-1] = words[index-1] + words[index]
+// 	// 				words = append(words[:index], words[index+1:]...)
+// 	// 			} else if index < len(words) {
+// 	// 				words[index-1] = words[index-1] + words[index]
+// 	// 				words = words[:index]
+// 	// 			}
+// 	// 		}
+// 	// 	}
+// 	// }
+
+// 	//return strings.Join(words, " ")
+
+// }
 
 func PairSpeechMark(s string) string {
-	words := strings.Split(s, " ")
-	isStartSpeechMark := false
-	for index, word := range words {
-		if isSpeechMark(word) {
-			isStartSpeechMark = !isStartSpeechMark
-			if isStartSpeechMark && index-1 > 0 && index+1 < len(words) {
-				words[index] = words[index] + words[index+1]
-				words = append(words[:index+1], words[index+2:]...)
-			} else if !isStartSpeechMark && index-1 > 0 {
-				if index+1 < len(words) {
-					words[index-1] = words[index-1] + words[index]
-					words = append(words[:index], words[index+1:]...)
-				} else if index < len(words) {
-					words[index-1] = words[index-1] + words[index]
-					words = words[:index]
-				}
-			}
-		}
-	}
-
-	return strings.Join(words, " ")
+	res := regexp.MustCompile(`'\s+(.*?)\s+'`)
+	str2 := res.ReplaceAllString(s, "'$1'")
+	return str2
 }
